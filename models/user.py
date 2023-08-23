d90f107814035e635494f933e683db982c885b6d
mawonde <mawondep@gmail.com>
2023-08-23 13:03:25 -0700

    Fixed user file

--- models/user.py
+++ models/user.py
@@ -6,21 +6,22 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-     __tablename__ = "users"
+  """This class defines a user by various attributes"""
+  __tablename__ = "users"
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", cascade="all, delete", backref="user")
-        review = relationship("Review", cascade="all, delete", backref="user")
+  if getenv("HBNB_TYPE_STORAGE") == "db":
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", cascade="all, delete", backref="user")
+    review = relationship("Review", cascade="all, delete", backref="user")
 
-    else:
+  else:
 
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''

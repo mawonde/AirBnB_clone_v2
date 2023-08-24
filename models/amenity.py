2ad5eb66a63582570b714d667d3bea68605436cc
junior sibusisos ngondo <juniorthemba1@gmail.com>
2023-08-24 12:43:54 -0700

    class name error

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
+
 """ State Module for HBNB project """
-from models.state import State
-=======
-""" Amenity Module for HBNB project """
-from models.base_model import BaseModel, Base
-from models.place import place_amenity
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
->>>>>>> ffc82477a0325a5b407471c8b87f23afb4beef9b
 
 from os import getenv
 from models.base_model import BaseModel, Base
@@ -16,7 +8,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 # from sqlalchemy.orm import relationship
 
-<<<<<<< HEAD
+
 
 class Amenity(BaseModel, Base):
   """ The amenity class """
@@ -29,13 +21,3 @@ class Amenity(BaseModel, Base):
 
   else:
     name = ""
-=======
-class Amenity(BaseModel, Base):
-  __tablename__ = 'amenities'
-
-  name = Column(String(128), nullable=False)
-
-  place_amenities = relationship("Place",
-                                 secondary=place_amenity,
-                                 back_populates="amenities")
->>>>>>> ffc82477a0325a5b407471c8b87f23afb4beef9b

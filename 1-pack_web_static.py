aed512b717ed215ee340f34dee3f19373846ca44
mawonde <mawondep@gmail.com>
2023-09-10 09:56:02 -0700

    Fixed codestyle of 1-pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,13 +6,13 @@ import os
 
 
 def do_pack():
-    try:
-        if not os.path.exists("versions"):
-            local('mkdir versions')
-        t = datetime.now()
-        f = "%Y%m%d%H%M%S"
-        path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
-        local('tar -cvzf {} web_static'.format(path))
-        return path
-    except:
-        return None
+  try:
+    if not os.path.exists("versions"):
+      local('mkdir versions')
+    t = datetime.now()
+    f = "%Y%m%d%H%M%S"
+    path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+    local('tar -cvzf {} web_static'.format(path))
+    return path
+  except:
+    return None
